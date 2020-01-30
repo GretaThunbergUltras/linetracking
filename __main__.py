94e33c9ebdb1fd1e5c7d97bd940eb82b3aa51684
witling <noreply@my.email>
2020-01-30 10:48:13 +0100

    add working version

--- __main__.py
+++ __main__.py
@@ -1,64 +1,143 @@
-#!/usr/bin/python3
-
-import cv2 as cv
+import cv2
 import numpy as np
+import time
+import brickpi3
 
-RESOLUTION = (800, 600)
-# (x, y, w, h)
-ROI_W = RESOLUTION[0]//3
-ROI_H = RESOLUTION[1]//2 
-ROI_X = RESOLUTION[0]//2 - ROI_W//2
-ROI_Y = RESOLUTION[1] - ROI_H
+line_coordinates = 0, 0, 0, 0
 
-ROI_X2 = ROI_X + ROI_W
-ROI_Y2 = ROI_Y + ROI_H
+def region_of_interest(edges2):
+    height, width = edges2.shape
+    print(width)
+    print(height)
+    mask2 = np.zeros_like(edges2)
 
-def contours(img):
-    ret, thresh = cv.threshold(img, 127, 255, 0)
-    contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    edges = cv.Canny(img, 100, 200)
-    cv.drawContours(img, contours, -1, (0, 255, 0), 3)
+    # only focus bottom half of the screen
+    polygon = np.array([[
+        (width * 1/3, height * 2 / 3),
+        (width * 2/3, height * 2 / 3),
+        (width * 2/3, height),
+        (width * 1/3, height),
+    ]], np.int32)
 
-def canny(img):
-    edges = cv.Canny(img, 50, 150, 3)
-    lines = cv.HoughLinesP(edges, 1, np.pi/180, 200)
-    if not isinstance(lines, np.ndarray):
-        return
-    for rho, theta, _, _ in lines[0]:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
+    cv2.fillPoly(mask2, polygon, 255)
+    cropped_edges2 = cv2.bitwise_and(edges2, mask2)
+    return cropped_edges2
 
-        cv.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+def detect_line_segments(cropped_edges2):
+    # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
+    rho = 1  # distance precision in pixel, i.e. 1 pixel
+    angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+    min_threshold = 30  # minimal of votes
+    line_segments2 = cv2.HoughLinesP(cropped_edges2, rho, angle, min_threshold,
+                                    np.array([]), minLineLength=8, maxLineGap=1)
+    return line_segments2
 
-def main():
-    cap = cv.VideoCapture('test.mp4')
+def average_slope_intercept(frame2, line_segments2):
+    lane_lines = []
+    if line_segments2 is None:
+        return lane_lines
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+    height, width, _ = frame2.shape
+    left_fit = []
 
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    for line_segment in line_segments2:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2:
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            left_fit.append((slope, intercept))
 
-        roi = frame[ROI_Y:ROI_Y2, ROI_X:ROI_X2]
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
 
-        contours(roi)
-        # canny(roi)
+        lane_lines.append(make_points(frame2, left_fit_average))
 
-        cv.rectangle(frame, (ROI_X, ROI_Y), (ROI_X2, ROI_Y2), (255, 0, 0))
+    return lane_lines
 
-        cv.imshow('preview', frame)
-        if cv.waitKey(0) == 27:
-            break;
+def make_points(frame3, line):
+    height, width, _ = frame3.shape
+    slope, intercept = line
+    y1 = height  # bottom of the frame
+    y2 = int(y1 * 2 / 3)  # make points from middle of the frame down
 
-    cv.destroyAllWindows()
-    cap.release()
+    # bound the coordinates within the frame
+    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+    global line_coordinates
+    line_coordinates = [x1, y1, x2, y2]
+    return [[x1, y1, x2, y2]]
 
-if __name__ == '__main__':
-    main()
+def display_lines(frame, lines, line_color=(0, 255, 0), line_width=4):
+    line_image = np.zeros_like(frame)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
+    line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    return line_image
+
+def line_tracking(line_coordinates, edges):
+    x1, y1, x2, y2 = line_coordinates
+    height, width = edges.shape
+
+    bottom_center = width * 1/2
+    delta = bottom_center - x1
+    if delta > -100 and delta < 100:
+        if (delta < -10):
+            print(bottom_center - x1)
+            BP.set_motor_power(BP.PORT_B, 20)
+            BP.set_motor_position(BP.PORT_D, 100)
+
+
+        elif delta > 10:
+            print(bottom_center - x1)
+            BP.set_motor_power(BP.PORT_B, 30)
+            BP.set_motor_position(BP.PORT_D, -20)
+
+        else:
+            print(bottom_center - x1)
+            BP.set_motor_power(BP.PORT_B, 30)
+            BP.set_motor_position(BP.PORT_D, 0)
+    else:
+        print(bottom_center - x1)
+        BP.set_motor_power(BP.PORT_B, 0)
+        BP.set_motor_position(BP.PORT_D, 0)
+
+
+BP = brickpi3.BrickPi3()
+
+cap = cv2.VideoCapture(0)
+
+while (True):
+
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_black = np.array([0, 0, 0])
+    upper_black = np.array([255, 100, 40])
+    mask = cv2.inRange(hsv, lower_black, upper_black)
+    edges = cv2.Canny(mask, 100, 200)
+    cropped_edges = region_of_interest(edges)
+    line_segments = detect_line_segments(cropped_edges)
+    lane_lines2 = average_slope_intercept(frame, line_segments)
+    cv2.imshow('edges', edges)
+    cv2.imshow('cropped_edges', cropped_edges)
+    lane_lines_image = display_lines(frame, lane_lines2)
+    cv2.imshow("lane lines", lane_lines_image)
+
+    #cv2.imshow('frame', frame)
+    #cv2.imshow('mask', mask)
+
+    print(line_coordinates)
+
+    line_tracking(line_coordinates, edges)
+
+    time.sleep(0.1)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        BP.set_motor_power(BP.PORT_B, 0)
+        BP.set_motor_position(BP.PORT_D, 0)
+        break
+
+cv2.destroyAllWindows()
