8862e1e264a95c35a9f7f2dd8fe5cb5efc45fb9d
lausek <root@lausek.eu>
2020-03-04 16:54:57 +0100

    test refactoring

--- test.py
+++ test.py
@@ -1,19 +1,21 @@
+import cv2
 import sys
 
-import cv2
+def main():
+    cap = cv2.VideoCapture('simpleline.mp4')
+    
+    if '--lr' in sys.argv:
+        from lr_tracking import LRTracker
+        ln = LRTracker(cap)
+    else:
+        from contours_tracking import ContourTracker
+        ln = ContourTracker(cap)
+    
+    while True:
+        ln.track_line()
+        cv2.waitKey(1)
+    
+    cv2.closeAllWindows()
 
-cap = cv2.VideoCapture('simpleline.mp4')
-
-if '--lr' in sys.argv:
-    from lr_tracking import LRTracker
-    ln = LRTracker(cap)
-else:
-    from contours_tracking import ContourTracker
-    ln = ContourTracker(cap)
-
-while True:
-    ln.track_line()
-    cv2.waitKey(1)
-
-test.release()
-cv2.closeAllWindows()
+if __name__ == '__main__':
+    main()
